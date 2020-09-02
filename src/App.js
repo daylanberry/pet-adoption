f71180c3564d4a9d1ebb9b189b9f5d0d3e9c0bdf
Daylan <daylanberry@gmail.com>
2020-09-02 09:09:45 -0700

    created get breeds and type of animal component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SearchParams from './SearchParams';
+import { Router, Link } from '@reach/router';
+import Details from './Details';
+
+
+const App = () => {
+
+  return (
+    <div>
+      <header>
+        <Link to='/'>
+          Adopt Me!
+        </Link>
+      </header>
+      <Router>
+        <SearchParams path='/' />
+        <Details path='/details/:id' />
+      </Router>
+    </div>
+  )
+};
+
+render(<App />, document.getElementById("root"));
